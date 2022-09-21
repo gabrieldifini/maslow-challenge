443c310f87b41b2ecdbc1dc3434177d48a8f6af5
gabrieldifini <gabriel.di.fini@outlook.com>
2022-09-21 04:08:52 -0300

    fixes

--- pages/components/ChartCard.js
+++ pages/components/ChartCard.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
-import { getColorFromStyles } from "../utils/utils";
+import { getColorFromStyles } from "./utils/utils";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
