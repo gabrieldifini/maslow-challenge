c81f5f2a5afbc2d8de8fbeeb9e9aa0f03a4b0c7e
Gabriel Di Fini <gabriel.di.fini@outlook.com>
2022-09-21 15:53:24 -0300

    removed unnecesary code

--- components/TotalsCard.js
+++ components/TotalsCard.js
@@ -1,8 +1,3 @@
-import { useState, useEffect } from "react";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-
-ChartJS.register(ArcElement, Tooltip, Legend);
-
 const TotalsCard = ({ remainder = 0, total = 0, currency = "ARS" }) => {
   return (
     <div className="border-solid h-auto border border-gray-100 rounded-tr-xl rounded-bl-xl p-6 relative shadow-md h-fit">
